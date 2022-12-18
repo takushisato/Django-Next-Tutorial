4f0cf7ac81c2e577e4c34842bf9329f8b7d185dd
taku820801 <takuchanpower@gmail.com>
2022-12-18 14:44:50 +0900

    設定変更

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_swagger',
     'djoser',
     'apps.account',
     'apps.app',
--- backend/server/urls.py
+++ backend/server/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
@@ -9,5 +8,4 @@ urlpatterns = [
     path('api/verify/', TokenVerifyView.as_view()),
     path('api/auth/', include('apps.account.urls')),
     path('admin/', admin.site.urls),
-    path('api/v1/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
 ]
